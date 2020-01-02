5d1652c79ea6312d8204cd8453cff6baf347acac
Zico <21520993@qq.com>
2020-01-02 10:54:43 +0800

    增加Guide和Post

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,31 @@
 const config = {
     title: 'Zico.kuo',
-    description: 'a blog for github pages by VuePress',themeConfig : {
-        nav : [ // 添加导航栏
+    description: 'a blog for github pages by VuePress',
+
+    plugins: [
+        '@vuepress/active-header-links',
+        '@vuepress/plugin-nprogress',
+        '@vuepress/plugin-register-components',
+        '@vuepress/plugin-back-to-top'
+    ],
+
+    markdown: {
+        anchor: {
+            // permalink: true,
+            // permalinkBefore: true,
+            permalinkSymbol: '#',
+        },
+        lineNumbers: true,
+        toc: { includeLevel: [1, 2, 3] },
+    },
+
+    themeConfig: {
+        nav: [ // 添加导航栏
+
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'Post', link: '/post/' },
+            { text: 'About', link: '/about/' },
             {
                 text: 'Languages',
                 items: [
@@ -12,8 +36,9 @@ const config = {
         ],
         // 添加侧边栏
         sidebar: 'auto',
-        sidebarDepth: 2
+        sidebarDepth: 3
     }
-}
+
+};
 
 module.exports = config;
--- docs/README.md
+++ docs/README.md
@@ -1,7 +1,13 @@
 # Zico.kuo Blog
 
+> a blog for record deving Q & A by Zico
+
+
 [toc]
 
 ---
+::: tip Footer
 
 Copy right by `Zico` & `VuePress`
+
+:::

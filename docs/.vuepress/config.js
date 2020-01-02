57108af2e6c3d4a8e906c18e7fea3ba27d21896a
Zico <21520993@qqcom>
2020-01-03 01:21:21 +0800

    123

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,54 +1,63 @@
 const config = {
-    title: 'Zico.kuo',
-    description: 'a blog for github pages by VuePress',
+  title: "Zico.kuo",
+  description: "a blog for github pages by VuePress",
 
-    plugins: [
-        '@vuepress/active-header-links',
-        '@vuepress/plugin-nprogress',
-        '@vuepress/plugin-register-components',
-        '@vuepress/plugin-back-to-top',
-        'vuepress-plugin-comment',
+  plugins: [
+    "@vuepress/active-header-links",
+    "@vuepress/plugin-nprogress",
+    "@vuepress/plugin-register-components",
+    "@vuepress/plugin-back-to-top",
+    "@vuepress/blog",
+    {
+      directories: [
         {
-          choosen: 'valine', 
-          // options选项中的所有参数，会传给Valine的配置
-          options: {
-            el: '#valine-vuepress-comment',
-            appId: 'Your own appId',
-            appKey: 'Your own appKey'
+          // Unique ID of current classification
+          id: "post",
+          // Target directory
+          dirname: "post",
+          // Path of the `entry page` (or `list page`)
+          path: "/",
+          layout: "/post/theme/Layout",
+          itemLayout: "MyPost",
+          sitemap: {
+            hostname: "https://zicokuo.github.io"
           }
         }
-    ],
-
-    markdown: {
-        anchor: {
-            // permalink: true,
-            // permalinkBefore: true,
-            permalinkSymbol: '#',
-        },
-        lineNumbers: true,
-        toc: { includeLevel: [1, 2, 3] },
+      ]
     },
+   
+  ],
 
-    themeConfig: {
-        nav: [ // 添加导航栏
+  markdown: {
+    anchor: {
+      // permalink: true,
+      // permalinkBefore: true,
+      permalinkSymbol: "#"
+    },
+    lineNumbers: true,
+    toc: { includeLevel: [1, 2, 3] }
+  },
 
-            { text: 'Home', link: '/' },
-            { text: 'Sets', link: '/sets/' },
-            { text: 'Post', link: '/post/' },
-            { text: 'About', link: '/about/' },
-            {
-                text: 'Languages',
-                items: [
-                    { text: 'Chinese', link: '/language/chinese/' },
-                    { text: 'Japanese', link: '/language/japanese/' }
-                ]
-            }
-        ],
-        // 添加侧边栏
-        sidebar: 'auto',
-        sidebarDepth: 3
-    }
+  themeConfig: {
+    nav: [
+      // 添加导航栏
 
+      { text: "Home", link: "/" },
+      { text: "Sets", link: "/sets/" },
+      { text: "Post", link: "/post/" },
+      { text: "About", link: "/about/" },
+      {
+        text: "Languages",
+        items: [
+          { text: "Chinese", link: "/language/chinese/" },
+          { text: "Japanese", link: "/language/japanese/" }
+        ]
+      }
+    ],
+    // 添加侧边栏
+    sidebar: "auto",
+    sidebarDepth: 3
+  }
 };
 
 module.exports = config;
---
+++ docs/post/theme/Layout.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <categoryNavigator></categoryNavigator>
+    <Content></Content>
+  </div>
+</template>

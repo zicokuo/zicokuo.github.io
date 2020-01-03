09b31b2d5423e6a3a8806d83f1a1f44d356a0833
Zico <21520993@qq.com>
2020-01-03 16:54:25 +0800

    升级栏目路由

--- docs/.vuepress/components/categoryNavigator.vue
+++ docs/.vuepress/components/categoryNavigator.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
     <p>This Category has {{ $categoryRouter.length-1}} Posts</p>
-    
-    
+
+    <div
+      v-for="(route,index) in $categoryRouter"
+      :key="index"
+    >
+      <router-link
+        :to="route.path"
+        v-if="route.title"
+      >
+        No.{{index+1}} - {{route.title}}
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -17,7 +27,7 @@ export default {
       get() {
         let catePath = this.$page.path.split("/")[1];
         return this.$site.pages.filter(p => {
-          return new RegExp(`^\/${catePath}`, "is").test(p.path);
+          return new RegExp(`^\/${catePath}`, "is").test(p.path) && p.title;
         });
       }
     }
---
+++ docs/.vuepress/components/mixins/routerMixins.js
@@ -0,0 +1,7 @@
+const routerMixin = {
+    methods: {
+        parseRouteName: (route) => {
+            
+        }
+    }
+}
